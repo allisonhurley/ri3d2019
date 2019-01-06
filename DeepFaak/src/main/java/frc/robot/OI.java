1628016c4e18d7410aedf2fee2c3d3029dfcac56
Jesse Estes <jestes5111@gmail.com>
2019-01-05 21:23:29 -0500

    Drive code

--- DeepFaak/src/main/java/frc/robot/OI.java
+++ DeepFaak/src/main/java/frc/robot/OI.java
@@ -7,36 +7,37 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import frc.robot.utils.Gamepad;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+  /* Motor Controllers */
+  public WPI_TalonSRX driveLeftFront = new WPI_TalonSRX(7);
+  public WPI_TalonSRX driveLeftMiddle = new WPI_TalonSRX(6);
+  public WPI_TalonSRX driveLeftBack = new WPI_TalonSRX(5);
+  public WPI_TalonSRX driveRightFront = new WPI_TalonSRX(0);
+  public WPI_TalonSRX driveRightMiddle = new WPI_TalonSRX(1);
+  public WPI_TalonSRX driveRightBack = new WPI_TalonSRX(2);
+  public WPI_TalonSRX collectorOne = new WPI_TalonSRX(3);
+  public WPI_TalonSRX collectorTwo = new WPI_TalonSRX(4);
+  
+  /* Controllers */
+  Gamepad driverGamepad = new Gamepad(RobotMap.DRIVER_GAMEPAD_PORT);
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+  /*  */
+  public double getDriverGamepadLeftY() {
+    double y = driverGamepad.getLeftY();
+    return y;
+  }
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
-
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
-
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+  public double getDriverGamepadRightX() {
+    double x = driverGamepad.getRightX();
+    return x;
+  }
 }
--- DeepFaak/src/main/java/frc/robot/commands/TankDrive.java
+++ DeepFaak/src/main/java/frc/robot/commands/TankDrive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,13 +10,10 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class TankDrive extends Command {
+  public TankDrive() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    requires(Robot.drivetrain);
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +24,10 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.drivetrain.tankDrive(
+      Robot.oi.getDriverGamepadLeftY(),
+      Robot.oi.getDriverGamepadRightX()
+    );
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -38,11 +39,13 @@ public class ExampleCommand extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.drivetrain.stop();
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    end();
   }
 }
